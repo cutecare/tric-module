5141e53779e19bb91c8cf55a52521646f38ac0e2
CuteCare <33788776+cutecare@users.noreply.github.com>
2020-02-17 19:54:06 +0300

    Update tric-module.h

--- tric-module.h
+++ tric-module.h
@@ -15,7 +15,7 @@ Author: evgeny.savitsky@gmail.com
 
 class TRIC {
   public:
-	  TRIC(int rxPin = 3, int txPin = 4, int resetPin = 5);
+	  TRIC(int rxPin = 12, int txPin = 8, int resetPin = 13);
 	  void configure(const char * bleName, int io1 = 0, int io2 = 0, int io3 = 0, int io4 = 0);
 	  void setData(unsigned int minor = 0, unsigned int major = 0, short humidity = 0, short temperature = 0, short battery = 0, bool autosleep = true);
 	  void major(unsigned int value, bool autosleep = true);
@@ -23,6 +23,7 @@ class TRIC {
 	  void humidity(short value, bool autosleep = true);
 	  void temperature(short value, bool autosleep = true);
 	  void battery(short value, bool autosleep = true);
+	  void useRemoteXY();
 
   private:
 	  void sendCommand(const SoftwareSerial * bleSerial, const char * data);
@@ -32,6 +33,7 @@ class TRIC {
 	  int bleRxPin = 0;
 	  int bleResetPin = 0;
 	  long bleBaud = 9600;
+	  int remoteXYMode = 0;
 };
 
 #endif
